4e64b0ea33fdff644550e6a845c93ff197771096
Mohammad Najad <mjlr0909@gmail.com>
2023-09-28 20:20:43 -0400

    seed spot

--- backend/db/seeders/20230926000111-demo-user.js
+++ backend/db/seeders/20230926000111-demo-user.js
@@ -1,43 +1,88 @@
-'use strict';
+"use strict";
 
-const { User } = require('../models');
+const { User, Spot } = require("../models");
 const bcrypt = require("bcryptjs");
 
 let options = {};
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
 }
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await User.bulkCreate([
-      { firstName:"Joe",
-        lastName:"Smoe",
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password')
-      },
-      {
-        firstName:"Joey",
-        lastName:"Smoey",
-        email: 'user1@user.io',
-        username: 'FakeUser1',
-        hashedPassword: bcrypt.hashSync('password2')
-      },
-      {firstName:"Joerrr",
-      lastName:"Smoerrr",
-        email: 'user2@user.io',
-        username: 'FakeUser2',
-        hashedPassword: bcrypt.hashSync('password3')
-      }
-    ], { validate: true });
+  async up(queryInterface, Sequelize) {
+    await User.bulkCreate(
+      [
+        {
+          firstName: "Joe",
+          lastName: "Smoe",
+          email: "demo@user.io",
+          username: "Demo-lition",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          firstName: "Joey",
+          lastName: "Smoey",
+          email: "user1@user.io",
+          username: "FakeUser1",
+          hashedPassword: bcrypt.hashSync("password2"),
+        },
+        {
+          firstName: "Joerrr",
+          lastName: "Smoerrr",
+          email: "user2@user.io",
+          username: "FakeUser2",
+          hashedPassword: bcrypt.hashSync("password3"),
+        },
+      ],
+      { validate: true }
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    options.tableName = 'Users';
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
-    }, {});
-  }
+    return queryInterface.bulkDelete(
+      options,
+      {
+        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
+      },
+      {}
+    );
+  },
+};
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await Spot.bulkCreate(
+      [
+        {
+          address: "123 fish st",
+          city: "atlantis",
+          state: "Timbucktoo",
+          country: "france",
+          lat: 12.45,
+          lng: 11.1,
+          name: "bikini bottom",
+          description: "where spongebob lives",
+          price: 250,
+        },
+        {
+          address: "900 fish st",
+          city: "chicago",
+          state: "illinois",
+          country: "USA",
+          lat: 12.47,
+          lng: 11.13,
+          name: "OG Fish Spot",
+          description: "only og fishes live here ",
+          price: 300,
+        },
+      ],
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Spots", null, {});
+  },
 };
