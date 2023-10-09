3fd9141f79b72379c0d0b3911511126c1219af2a
Mohammad Najad <mjlr0909@gmail.com>
2023-10-09 03:00:19 -0400

    s

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -178,12 +178,12 @@ router.get("/current", requireAuth, async (req, res, next) => {
       "createdAt",
       "updatedAt",
 
-      [
-        sequelize.literal(
-          `(SELECT "url" FROM "Images" AS "SpotImages" WHERE "SpotImages"."spotId" = "Spot"."id" AND "SpotImages"."preview" = true LIMIT 1)`
-        ),
-        "previewImage",
-      ],
+      // [
+      //   sequelize.literal(
+      //     `(SELECT "url" FROM "Images" AS "SpotImages" WHERE "SpotImages"."spotId" = "Spot"."id" AND "SpotImages"."preview" = true LIMIT 1)`
+      //   ),
+      //   "previewImage",
+      // ],
       // [
       //   sequelize.literal(
       //     `(SELECT AVG("stars") FROM "Review" WHERE "Review"."spotId" = "Spot"."id")`
@@ -194,7 +194,10 @@ router.get("/current", requireAuth, async (req, res, next) => {
     include: [
       {
         model: SpotImage,
-        attributes: [],
+        as: 'images',
+            attributes: ["url"],
+            where: { preview: true },
+            required: false,
       },
       {
         model: Review,
@@ -205,15 +208,15 @@ router.get("/current", requireAuth, async (req, res, next) => {
 
 
   const spots = await Spot.findAll(filter);
-  // for (const spot of spots) {
-  //   const avgRating = await Review.findOne({
-  //     where: { spotId: spot.id },
-  //     attributes: [[sequelize.fn("avg", sequelize.col("stars")), "avgRating"]],
-  //     raw: true,
-  //   });
+  for (const spot of spots) {
+    const avgRating = await Review.findOne({
+      where: { spotId: spot.id },
+      attributes: [[sequelize.fn("avg", sequelize.col("stars")), "avgRating"]],
+      raw: true,
+    });
 
-  //   spot.dataValues.avgRating = avgRating?.avgRating || null;
-  // }
+    spot.dataValues.avgRating = avgRating?.avgRating || null;
+  }
 
   return res.json({ Spots: spots });
 });
