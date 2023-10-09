3809e53edc300d38dbc2dc2d0ffe889691ac08de
Mohammad Najad <mjlr0909@gmail.com>
2023-10-09 16:10:33 -0400

    pushed

--- backend/db/migrations/20230928231715-create-booking.js
+++ backend/db/migrations/20230928231715-create-booking.js
@@ -21,7 +21,7 @@ module.exports = {
           model: 'Spots',
           key: 'id',
         },
-        onDelete: 'cascade',
+        onDelete: 'CASCADE',
       },
       userId: {
         type: Sequelize.INTEGER,
@@ -29,7 +29,7 @@ module.exports = {
           model: 'Users',
           key: 'id',
         }
-        , onDelete: 'cascade',
+        , onDelete: 'CASCADE',
       },
       startDate: {
         type: Sequelize.DATE
